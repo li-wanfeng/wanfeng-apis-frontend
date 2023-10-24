e7ab23916abe1771b3adf0697b9cdd12cd45b07b
lidechengguo <wanshishengyi33@163.com>
2023-10-24 23:30:55 +0800

    接口详情页面，测试调用接口并展示返回值

--- src/components/InterfaceInfo/requestParams.tsx
+++ src/components/InterfaceInfo/requestParams.tsx
@@ -1,54 +1,61 @@
 import React, {useEffect, useState} from "react";
-import {Input, Button, Card, Col} from 'antd';
+import {Card, Col, Input, Spin} from 'antd';
 import TextArea from "antd/es/input/TextArea";
 import Search from "antd/es/input/Search";
-const RequestParams: React.FC<{data:wfAPI.InterfaceInfoVO}> = ({data}) => {
-    const [requestParam, setRequestParam] = useState('');
-    const [reMethod, setReMethod] = useState('');
-    const [responceContent,setResponceContent] = useState('')
-    useEffect(()=>{
+import {interfaceInfoinvokeUsingPOST} from "@/services/wfAPI/jiekoumokuai";
+
+const RequestParams: React.FC<{ data: wfAPI.InterfaceInfoVO }> = ({data}) => {
+    const [responceContent, setResponceContent] = useState('')
+    const [params, setParams] = useState('')
+    const [loading,setLoading] = useState(false)
+    useEffect(() => {
         if (data.requestparams && data.requestparams.length > 0) {
             const paseParams = JSON.parse(data.requestparams);
             const formatted = JSON.stringify(paseParams, null, 2);
-            setRequestParam(formatted);
+            setParams(formatted);
         }
-        if (data.method && data.method.length >=0) {
-            setReMethod(data.method);
+    }, [])
+    const handleParamsChange = (event:any) => {
+        setParams(event.target.value)
+    };
+    const handleSendClick = async () => {
+        setLoading(true)
+        try {
+            const res = await interfaceInfoinvokeUsingPOST({
+                id: data.id, method: data.method, requestparams: JSON.parse(params), url: data.url, requesttype: data.requesttype
+            })
+            setResponceContent(JSON.stringify(res.data,null,2))
+        } catch (e) {
+        } finally {
+            setLoading(false)
         }
-    },[{...data}])
-    const handleParamsChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setRequestParam(e.target.value);
+
     };
-    const handleSendClick = () => {
-        // 处理发送请求的逻辑
-    };
-    return(
-        <Col span={12}>
+    return (<Col span={12}>
             <Card>
                 <Search
-                    addonBefore={<span style={{fontWeight:'bold'}}>{reMethod.toUpperCase()}  请求</span>}
+                    addonBefore={<span style={{fontWeight: 'bold'}}>{data?.method?.toUpperCase()} 请求</span>}
                     defaultValue={`${data.url}`}
                     allowClear
                     onSearch={handleSendClick}
                     enterButton="发送"
-                    style={{ width: '100%' }}
+                    style={{width: '100%'}}
                 />
-                <div style={{margin:20}}><span style={{fontWeight:'bold'}}>请求参数</span></div>
-                <div style={{ display: 'flex', marginBottom: 16 }}>
-                    <div style={{ flex: 1 }}>
-                        <Input.TextArea
-                            value={requestParam}
-                            onChange={handleParamsChange}
-                            autoSize={{ minRows: 6, maxRows: 12 }}
-                        />
+                <div style={{margin: 20}}><span style={{fontWeight: 'bold'}}>请求参数</span></div>
+                <div style={{display: 'flex', marginBottom: 16}}>
+                    <div style={{flex: 1}}>
+                            <Input.TextArea
+                                value={params}
+                                onChange={handleParamsChange}
+                                autoSize={{minRows: 6, maxRows: 12}}
+                            />
                     </div>
-                    <div style={{ marginLeft: 16 }}>
+                    <div style={{marginLeft: 16}}>
                     </div>
                 </div>
-                <div style={{margin:20}}><span style={{fontWeight:'bold'}}>响应内容</span></div>
-                <TextArea value={responceContent} autoSize={{ minRows: 10 }}/>
+                <div style={{margin: 20}}><span style={{fontWeight: 'bold'}}>响应内容</span></div>
+                <Spin spinning={loading}><TextArea value={responceContent} autoSize={{minRows: 10}}/></Spin>
             </Card>
-        </Col>
-    )
+        </Col>)
 };
 export default RequestParams;
